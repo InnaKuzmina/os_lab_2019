49e1bc4a7bdc8ab8559f8fb723328a8effbf4e04
Inna <kuzmina.inno@gmail.com>
2019-03-11 18:35:43 +0000

    добавлен revert_string.c

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,11 @@
 #include "revert_string.h"
 
+
 void RevertString(char *str)
 {
-	// your code here
+	int length = strlen(str); 
+    char mystr[length];
+    for (int i = 0; i < length; i++)  mystr[i] = str[length-i-1]; 
+    for (int i = 0; i < length; i++)  str[i] = mystr[i]; 
 }
 
